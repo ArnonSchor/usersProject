335ebf50b5822d70a7b898f33aef7807327a2f48
Arnon <arnon@appricots.onmicrosoft.com>
2023-10-03 14:46:16 +0300

    initial commit

---
+++ front/usersProject/src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:2000")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+  return (
+    <>
+      <h1>{message}</h1>
+    </>
+  );
+}
+export default App;
