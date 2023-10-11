ccfe594150c29e0de70b5fa0a6fa3a7e2b1c614b
Arnon Schor <schorarnon@gmail.com>
2023-10-11 18:21:06 +0300

    added formik and yup, need to finish yup

--- front/usersProject/src/components/SignupForm.tsx
+++ front/usersProject/src/components/SignupForm.tsx
@@ -2,7 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { useNavigate } from "react-router-dom";
 import axiosInstance from "../axiosInstance";
 import { useFormik } from "formik";
+import * as Yup from "yup";
 
+interface FormValues {
+  username: string;
+  password: string;
+  confirmPassword: string;
+  email: string;
+}
 interface Props {
   route: string;
 }
@@ -10,16 +17,32 @@ interface Props {
 export const SignupForm = ({ route }: Props) => {
   const navigate = useNavigate();
 
-  const formik = useFormik({
-    initialValues: {
-      username: "",
-      password: "",
-      email: "",
-    },
-    onSubmit: async (values) => {
+  const initialValues = {
+    username: "",
+    password: "",
+    confirmPassword: "",
+    email: "",
+  };
+  const onSubmit = async (values: FormValues) => {
+    if (values.password === values.confirmPassword) {
       await axiosInstance.post("signUp", { ...values });
       navigate(route);
-    },
+    } else {
+      alert("passwords do not match!");
+    }
+  };
+  const validationSchema = Yup.object({
+    username: Yup.string().required("Required"),
+    password: Yup.string().required("Required"),
+    confirmPassword: Yup.string().required("Required"),
+    email: Yup.string()
+      .email("please enter a valid email")
+      .required("Required"),
+  });
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validationSchema,
   });
 
   return (
@@ -30,15 +53,27 @@ export const SignupForm = ({ route }: Props) => {
         placeholder="username"
         type="text"
         onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
         value={formik.values.username}
       />
+
       <input
         required
         name="password"
         placeholder="password"
         type="password"
         value={formik.values.password}
+        onBlur={formik.handleBlur}
         onChange={formik.handleChange}
+      />
+      <input
+        required
+        name="confirmPassword"
+        placeholder="confirm password"
+        type="password"
+        value={formik.values.confirmPassword}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
       />
       <input
         required
@@ -47,6 +82,7 @@ export const SignupForm = ({ route }: Props) => {
         type="email"
         value={formik.values.email}
         onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
       />
 
       <button type="submit">Submit</button>
